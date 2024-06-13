698725b3c01a30b41b5f52b62db470417da98b9a
Cristian Alfredo Manzo Parra <ca.manzo973@uniandes.edu.co>
2024-06-13 01:01:33 -0500

    feat style dashboard layout

--- web/src/layouts/DashboardLayout/DashboardLayout.tsx
+++ web/src/layouts/DashboardLayout/DashboardLayout.tsx
@@ -1,12 +1,50 @@
+import { NavLink, Link, routes } from '@redwoodjs/router'
+import { menuRoutes } from 'src/router/router'
+
 type DashboardLayoutProps = {
   children?: React.ReactNode
 }
 
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
-  return <>
-  <h1>DashboardLayout</h1>
-  {children}
-  </>
+  return (
+    <main className="mt-7 flex flex-row">
+      <nav className="ml-5 hidden min-h-[calc(100vh-3.0rem)] w-[370px] flex-col rounded-3xl bg-white bg-opacity-10 p-5 sm:flex">
+        <h1 className="bg-gradient-to-br from-white via-white/50 bg-clip-text text-lg font-bold text-transparent lg:text-3xl">
+          ReactGPT<span className="text-indigo-500">.</span>
+        </h1>
+        <span className="text-xl">Bienvenido</span>
+
+        <div className="my-3 border border-gray-700" />
+
+        {/* Opciones del menú */}
+        {menuRoutes.map((option) => (
+          <NavLink
+            key={option.to}
+            to={option.to}
+            className="flex items-center justify-center rounded-md p-2 transition-colors hover:bg-gray-800"
+            activeClassName="bg-gray-800"
+          >
+            {/* icon */}
+            <i className={`${option.icon} mr-4 text-2xl text-indigo-400`}></i>
+            <div className="flex flex-grow flex-col">
+              <span className="text-lg font-semibold text-white">
+                {option.title}
+              </span>
+              <span className="text-sm text-gray-400">
+                {option.description}
+              </span>
+            </div>
+          </NavLink>
+        ))}
+      </nav>
+
+      <section className="mx-3 flex h-[calc(100vh-50px)] w-full flex-col rounded-3xl  bg-white bg-opacity-10 p-5 sm:mx-20">
+        <div className="flex h-full flex-row">
+          <div className="flex h-full flex-auto flex-col p-1">{children}</div>
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default DashboardLayout
--- web/src/router/router.tsx
+++ web/src/router/router.tsx
@@ -1,67 +1,58 @@
+import { routes } from '@redwoodjs/router'
+
 export const menuRoutes = [
   {
-    to: "/orthography",
-    icon: "fa-solid fa-spell-check",
-    title: "Ortografía",
-    description: "Corregir ortografía",
-    component: <OrthographyPage />
+    to: '/orthography',
+    icon: 'fa-solid fa-spell-check',
+    title: 'Ortografía',
+    description: 'Corregir ortografía',
   },
   {
-    to: "/pros-cons",
-    icon: "fa-solid fa-code-compare",
-    title: "Pros & Cons",
-    description: "Comparar pros y contras",
-    component: <ProsConsPage />
+    to: '/pros-cons',
+    icon: 'fa-solid fa-code-compare',
+    title: 'Pros & Cons',
+    description: 'Comparar pros y contras',
   },
   {
-    to: "/pros-cons-stream",
-    icon: "fa-solid fa-water",
-    title: "Como stream",
-    description: "Con stream de mensajes",
-    component: <ProsConsStreamPage />
+    to: '/pros-cons-stream',
+    icon: 'fa-solid fa-water',
+    title: 'Como stream',
+    description: 'Con stream de mensajes',
   },
   {
-    to: "/translate",
-    icon: "fa-solid fa-language",
-    title: "Traducir",
-    description: "Textos a otros idiomas",
-    component: <TranslatePage />
+    to: '/translate',
+    icon: 'fa-solid fa-language',
+    title: 'Traducir',
+    description: 'Textos a otros idiomas',
   },
   {
-    to: "/text-to-audio",
-    icon: "fa-solid fa-podcast",
-    title: "Texto a audio",
-    description: "Convertir texto a audio",
-    component: <TextToAudioPage />
+    to: '/text-to-audio',
+    icon: 'fa-solid fa-podcast',
+    title: 'Texto a audio',
+    description: 'Convertir texto a audio',
   },
   {
-    to: "/image-generation",
-    icon: "fa-solid fa-image",
-    title: "Imágenes",
-    description: "Generar imágenes",
-    component: <ImageGenerationPage />
+    to: '/image-generation',
+    icon: 'fa-solid fa-image',
+    title: 'Imágenes',
+    description: 'Generar imágenes',
   },
   {
-    to: "/image-tunning",
-    icon: "fa-solid fa-wand-magic",
-    title: "Editar imagen",
-    description: "Generación continua",
-    component: <ImageTunningPage />
+    to: '/image-tunning',
+    icon: 'fa-solid fa-wand-magic',
+    title: 'Editar imagen',
+    description: 'Generación continua',
   },
   {
-    to: "/audio-to-text",
-    icon: "fa-solid fa-comment-dots",
-    title: "Audio a texto",
-    description: "Convertir audio a texto",
-    component: <AudioToTextPage />
+    to: '/audio-to-text',
+    icon: 'fa-solid fa-comment-dots',
+    title: 'Audio a texto',
+    description: 'Convertir audio a texto',
   },
   {
-    to: "/assistant",
-    icon: "fa-solid fa-user",
-    title: "Asistente",
-    description: "Información del asistente",
-    component: <AssistantPage />
+    to: '/assistant',
+    icon: 'fa-solid fa-user',
+    title: 'Asistente',
+    description: 'Información del asistente',
   },
-];
-
-
+]
